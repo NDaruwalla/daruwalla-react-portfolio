933dc44f0887e317aa507fddc56df122f8d9ada8
Nicole Daruwalla <nicole.daruwalla@gmail.com>
2021-07-29 11:11:32 -0400

    update resume section to enable users to click on View Resume PDF and view resume in a seperate tab

--- src/views/Resume/index.js
+++ src/views/Resume/index.js
@@ -1,29 +1,27 @@
 import React from "react";
 import "./style.css";
-// import Resume from "../../Components/ResumePDF";
 import Pdf from "../../components/ResumePDF/daruwalla-resume.pdf";
 
-function Resume() {
-  return (
-    <div className="viewResume" id="resume">
-    <div className="row">
-        <div className="col-md">
-              <div className="row align-self-center">
-                {/* <p className="resumeContainer"></p> */}
-               
-                <div onClick={() => window.open(Pdf)} className="viewTitle resumeTitle">RESUME</div>
-                
-                 {/* className= "viewTitle">RESUME</a> */}
-                {/* <div className="headerProfile-menu-list" onClick={() => window.open(pdfFile)}><i className="mdi mdi-help-circle"></i> Help</div> */}
-                {/* </p> */}
+function Skills() {
+    return (
+        <div className="viewResume" id="resume">
+            <div className="row">
+                <div className="col-md">
+                    <h3 className="viewTitle" id="skillsTitle">RESUME</h3>
+                    <div className="row align-self-center resumeText">
+                        <p className="resumeContainer">
+                         
+                            <ul className= "resumeLink">
+                            
+                                <li className="resSkill" onClick={() => window.open(Pdf)}>View Resume PDF</li>
+              
+                            </ul>
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-</div>
-)
+    )
 }
 
-
-
-
-export default Resume;
+export default Skills;
